e88c396605fd7a306319339a2fcd56bdc28cdd47
powerupminmin <imaihiromi.0101@gmail.com>
2022-01-13 17:40:14 +0900

    バリテーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category, :status, :charge, :area, :delivery_day
   has_one_attached :image
   
-  validates :name, :description, :price, presence: true
+  validates :image, :name, :description, :price, presence: true
   validates :category_id, :status_id, :charge_id, :area_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"} 
   
   validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
