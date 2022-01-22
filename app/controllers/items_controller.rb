28eec1fa937284303ec1c0e958128e900a90f829
powerupminmin <imaihiromi.0101@gmail.com>
2022-01-22 16:52:46 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -12,17 +13,30 @@ class ItemsController < ApplicationController
 
   def create
     @item =Item.new(item_params)
-    if @item.save
+  if @item.save
+      redirect_to root_path
+  else
+      render :new
+  end
+end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to root_path
     else
-      render :new
+      render :edit
     end
-
-    def show
-      
-    end
-
   end
+
 
   private
   
@@ -32,5 +46,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
