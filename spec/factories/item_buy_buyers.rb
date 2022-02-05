0356ea0073afbff2aa5d76a3729d2be49e380bcb
powerupminmin <imaihiromi.0101@gmail.com>
2022-02-05 17:33:40 +0900

    Formオブジェクトのテストコード作成

---
+++ spec/factories/item_buy_buyers.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item_buy_buyer do
+    post_code {'123-4567'}
+    area_id {14}
+    city {'渋谷区'}
+    address {'1-1'}
+    building_name {'ハイウェービル'}
+    phone_number {'09012345678'}
+  end
+end
